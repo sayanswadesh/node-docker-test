2f57869139a9dd90f9c98268376254390ffb7bfc
devbuilder07 <117940876+devbuilder07@users.noreply.github.com>
2022-11-19 16:10:22 +0530

    first api

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-console.log('Welcome to nodejs world!');
+console.log("Welcome to nodejs world!");
+
+app.get("/", function (req, res) {
+  res.send("Hello");
+});
 
 app.listen(4000, () => {
   console.log(`Server Running On 4000`);
