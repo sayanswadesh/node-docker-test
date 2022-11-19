8540ffb20f31ce893f6d6d1fef565783b63d51b2
devbuilder07 <117940876+devbuilder07@users.noreply.github.com>
2022-11-19 22:36:13 +0530

    message change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 console.log("Welcome to nodejs world!");
 
 app.get("/", function (req, res) {
-  res.send("Hello");
+  res.send("Welcome to nodejs api!!!");
 });
 
 app.listen(4000, () => {
